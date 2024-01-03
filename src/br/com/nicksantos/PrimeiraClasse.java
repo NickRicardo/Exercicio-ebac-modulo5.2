6906616bc5348187496b86e152799addd8acf3a7
Nícolas Santos <nicollas.ricardo@hotmail.com>
2024-01-03 07:29:56 -0300

    primeiro commit

---
+++ src/br/com/nicksantos/PrimeiraClasse.java
@@ -0,0 +1,11 @@
+package br.com.nicksantos;
+
+public class PrimeiraClasse{
+
+    public static void main (String [] args){
+        System.out.println("***********************************************************************");
+        System.out.println("Hello Word!");
+        System.out.println("Aqui estarei aprendendo Java nesta ferramenta incrível que é o InteliJ");
+        System.out.println("***********************************************************************");
+    }
+}
